b0a50336bb716d380cce2201b3c101727a54a34a
carloruiz-nowarning <carlorg@live.com>
2026-08-26 10:53:54 -0400

    EOS quiz

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/EOSquiz', destination: '/EOSquiz.html' },
+    ];
+  },
+};
 
 export default nextConfig;
